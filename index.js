f0321c4941d1b6e18099165727940b95d3d00b9f
abetomo <abe@enzou.tokyo>
2017-05-30 17:24:01 +0900

    Add index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+'use srict'
+const path = require('path')
+module.exports = require(path.join(__dirname, 'lib', 'ss_to_json'))
---
+++ test/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const assert = require('chai').assert
+const Ss2Json = require('.')
+
+/* global describe, it */
+describe('Ss2Json', () => {
+  it('Ss2Json is instanceOf Object', () => {
+    assert.instanceOf(Ss2Json, Object)
+  })
+
+  it('More tests')
+})
