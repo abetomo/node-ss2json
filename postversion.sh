6ffab7e455e83a15927d9a88b78cd0c95ab76885
abetomo <abe@enzou.tokyo>
2017-05-31 19:43:29 +0900

    Add processing to push release branch

--- postversion.sh
+++ postversion.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
 version=$(git tag -l | tail -n1 | sed -e s/^v//)
+branch="release/${version}"
+
+git checkout -b ${branch}
 sed -i -e "s/\(const packageVersion =\).\+/\1 '${version}'/" test/ss_to_json.js
 
 git add test/ss_to_json.js
 git commit -m "Commit upgrade to v${version}"
-git push origin $(git tag -l | tail -n1)
+git push origin ${branch}
