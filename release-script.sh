8b02caa3d2f6a5518b1d7017e021933854f0060c
abetomo <abe@enzou.tokyo>
2017-05-31 20:02:17 +0900

    Add release-script

--- release-script.sh
+++ release-script.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-version=$(git tag -l | tail -n1 | sed -e s/^v//)
+newversion=${1:-patch}
+version=$(npm --no-git-tag-version version ${newversion} | sed -e s/^v//)
 branch="release/${version}"
 
 git checkout -b ${branch}
@@ -9,3 +10,7 @@ sed -i -e "s/\(const packageVersion =\).\+/\1 '${version}'/" test/ss_to_json.js
 git add test/ss_to_json.js
 git commit -m "Commit upgrade to v${version}"
 git push origin ${branch}
+
+git tag -a v${version}
+
+echo "Please run `git push origin v${version}` after merging release branch."
