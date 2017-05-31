d7f2db84a9f2bf802b0a6ee579a4b597886b8858
abetomo <abe@enzou.tokyo>
2017-05-31 19:33:52 +0900

    Add processing to commit files that are automatically updated

--- postversion.sh
+++ postversion.sh
@@ -3,4 +3,6 @@
 version=$(git tag -l | tail -n1 | sed -e s/^v//)
 sed -i -e "s/\(const packageVersion =\).\+/\1 '${version}'/" test/ss_to_json.js
 
+git add test/ss_to_json.js
+git commit -m "Commit upgrade to v${version}"
 git push origin $(git tag -l | tail -n1)
