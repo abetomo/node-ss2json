b3d882fc9e79ee8b2b498ccd1e782a7d5e67ed66
abetomo <abe@enzou.tokyo>
2017-06-01 17:52:45 +0900

    Add processing to check before commit

--- release-script.sh
+++ release-script.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+red="\033[0;31m"
+green="\033[0;32m"
+blue="\033[0;34m"
+color_off="\033[0;39m"
+
+### checkout master
 git checkout master
 git pull
 
@@ -18,6 +24,38 @@ branch="release/${version}"
 git checkout -b ${branch}
 sed -i -e "s/\(const packageVersion =\).\+/\1 '${version}'/" test/ss_to_json.js
 
+### Confirm diff before committing
+echo ${green}
+echo "============"
+echo "$(git diff)"
+echo "============"
+echo ${color_off}
+
+while true;do
+  echo -n ${red}
+  echo "Please check the difference.\nIs it OK to go through git push with this difference?\nType yes or no."
+  echo ${color_off}
+  read answer
+  case $answer in
+    yes)
+      break
+      ;;
+    no)
+      echo ${blue}
+      echo "Please execute the following command after correcting\n" \
+        "% git add .\n" \
+        "% git commit -m 'Commit upgrade to v${version}'\n" \
+        "% git push origin ${branch}\n" \
+        "% git tag -a v${version} -m '${changelog}'\n"
+      echo ${color_off}
+      exit
+      ;;
+    *)
+      echo "cannot understand $answer.\n"
+      ;;
+  esac
+done
+
 ### git commit & git push
 git add test/ss_to_json.js package.json CHANGELOG.md
 git commit -m "Commit upgrade to v${version}"
@@ -26,4 +64,6 @@ git push origin ${branch}
 ### Add tags locally
 git tag -a v${version} -m "${changelog}"
 
+echo ${blue}
 echo "Please run 'git push origin v${version}' after merging release branch."
+echo ${color_off}
