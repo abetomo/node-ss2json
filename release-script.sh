289358bc3a52137414e3cc008d51d566b8db3954
abetomo <abe@enzou.tokyo>
2017-06-01 17:51:39 +0900

    Modify processing to get the current tag and modify message when adding tag

--- release-script.sh
+++ release-script.sh
@@ -8,10 +8,10 @@ version_option=${1:-patch}
 version=$(npm --no-git-tag-version version ${version_option} | sed -e s/^v//)
 
 ### Add to CHANGELOG.md
-current_version=$(npm version from-git | sed -e s/^v//)
+current_tag=$(git tag --sort=-taggerdate | head -1 | sed -e s/^v//)
 echo "## [${version}] - $(date +%Y-%m-%d)" >> CHANGELOG.md
 # Assumption that squash merging is done
-git log v${current_version}.. | grep '(#[0-9]*)' | sed -e 's/^\s*/- /g' >> CHANGELOG.md
+changelog=$(git log v${current_tag}.. | grep '(#[0-9]*)' | sed -e 's/^\s*/- /g' | tee -a CHANGELOG.md)
 
 ### Automatic correction of test code by creating branch
 branch="release/${version}"
@@ -24,6 +24,6 @@ git commit -m "Commit upgrade to v${version}"
 git push origin ${branch}
 
 ### Add tags locally
-git tag -a v${version} -m v${version}
+git tag -a v${version} -m "${changelog}"
 
 echo "Please run 'git push origin v${version}' after merging release branch."
