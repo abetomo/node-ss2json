ba2221f21d4eb2f73ba044f99bc3db290fa5fdcd
abetomo <abe@enzou.tokyo>
2017-06-01 17:50:19 +0900

    Fix to process with master branch

--- release-script.sh
+++ release-script.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+git checkout master
+git pull
+
 ### Increment version
 version_option=${1:-patch}
 version=$(npm --no-git-tag-version version ${version_option} | sed -e s/^v//)
