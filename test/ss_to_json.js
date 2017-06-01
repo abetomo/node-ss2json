5ef9c988fda66e2e5d75b9ff05e8a43e079eaf9a
Tomoaki Abe <abe@enzou.tokyo>
2017-06-01 18:11:27 +0900

    Commit upgrade to v0.0.9 (#12)

--- test/ss_to_json.js
+++ test/ss_to_json.js
@@ -11,7 +11,7 @@ describe('Ss2Json', () => {
   })
 
   it('version should be set', function () {
-    const packageVersion = '0.0.8'
+    const packageVersion = '0.0.9'
     assert.equal(Ss2Json.version(), packageVersion)
   })
 
