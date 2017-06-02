eb6c159eecace8b8f07054dc3b92f9094dc610b6
Tomoaki Abe <abe@enzou.tokyo>
2017-06-02 18:44:26 +0900

    Fix as it will not be released successfully (#22)

--- release-script.sh
+++ release-script.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-git clone git@github.com:abetomo/node-ss2json.git -b master
+branch=$(git symbolic-ref --short HEAD)
+
+git clone git@github.com:abetomo/node-ss2json.git -b ${branch}
 cd node-ss2json
 git config --global user.email "${GH_USER_EMAIL}"
 git config --global user.name "${GH_USER_NAME}"
@@ -23,5 +25,11 @@ git add test/ss_to_json.js package.json CHANGELOG.md
 git commit -m "Commit upgrade to v${version}"
 git push
 
+### Merge into master
+git checkout master
+git merge ${branch}
+git push
+
 cd ..
 git pull
+rm -rf github_deploy_key node-ss2json
