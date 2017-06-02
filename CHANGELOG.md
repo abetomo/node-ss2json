576b6cd27c7e2f4467a225f894aa84b1dd228e11
abetomo <abe@enzou.tokyo>
2017-06-02 08:43:08 +0000

    Commit upgrade to v0.0.11

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,4 @@
 - Add processing to create CHANGELOG.md  (#9)
 ## [0.0.10] - 2017-06-01
 - Add email. Because it was necessary (#13)
+## [0.0.11] - 2017-06-02
--- test/ss_to_json.js
+++ test/ss_to_json.js
@@ -11,7 +11,7 @@ describe('Ss2Json', () => {
   })
 
   it('version should be set', function () {
-    const packageVersion = '0.0.10'
+    const packageVersion = '0.0.11'
     assert.equal(Ss2Json.version(), packageVersion)
   })
 
