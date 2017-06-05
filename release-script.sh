b355d6f66e824fb56686645e2f32d4a279031fbd
abetomo <abe@enzou.tokyo>
2017-06-05 16:58:06 +0900

    Improve release script
    - Check version
    - Add comments

--- release-script.sh
+++ release-script.sh
@@ -1,27 +1,39 @@
 #!/usr/bin/env sh
 
-branch=${TRAVIS_BRANCH}
-echo ${branch}
-
-git clone git@github.com:abetomo/node-ss2json.git -b "${branch}"
-cd node-ss2json
+### git settings
 git config --global user.email "${GH_USER_EMAIL}"
 git config --global user.name "${GH_USER_NAME}"
 
-### Increment version
-version_option=${1:-patch}
-version=$(npm --no-git-tag-version version ${version_option} | sed -e s/^v//)
+### check version
+branch=${TRAVIS_BRANCH}
+echo ${branch}
+
+new_version=$(echo ${branch} | cut -d/ -f2)
+current_version=$(node -e 'console.log(require("./package.json").version)')
+if [ "${new_version}" = "${current_version}" ]; then
+    echo "[${new_version}] It is already released."
+    exit 0
+fi
+
+### Clone working repository
+echo 'Clone working repository'
+git clone git@github.com:abetomo/node-ss2json.git -b "${branch}"
+cd node-ss2json
+
+### Update version
+version=$(npm --no-git-tag-version version ${new_version} | sed -e s/^v//)
 
 ### Add to CHANGELOG.md
 current_tag=$(git for-each-ref --sort=-taggerdate --format='%(tag)' refs/tags | head -1 | sed -e s/^v//)
-echo "## [${version}] - $(date +%Y-%m-%d)" >> CHANGELOG.md
+echo "## [${version}] - $(date +%Y-%m-%d)" | tee -a CHANGELOG.md
 # Assumption that squash merging is done
 changelog=$(git log v${current_tag}.. | grep '(#[0-9]*)' | sed -e 's/^\s*/- /g' | sed -e '$d' | tee -a CHANGELOG.md)
+echo ${changelog}
 
 ### Automatic correction of test code
 sed -i -e "s/\(const packageVersion =\).\+/\1 '${version}'/" test/ss_to_json.js
 
-### git commit & git push
+### git commit & git push On the release branch
 git add test/ss_to_json.js package.json CHANGELOG.md
 git commit -m "Commit upgrade to v${version}"
 git push
@@ -31,6 +43,7 @@ git checkout master
 git merge ${branch}
 git push
 
+### Tagging
 git tag -a v${version} -m "${changelog}"
 git push origin v${version}
 
