6c48eacb2ab00556506d86187da971fcba23876d
abetomo <abe@enzou.tokyo>
2017-07-06 07:08:48 +0000

    Commit upgrade to v0.1.0

--- test/ss_to_json.js
+++ test/ss_to_json.js
@@ -11,7 +11,7 @@ describe('Ss2Json', () => {
   })
 
   it('version should be set', function () {
-    const packageVersion = '0.0.25'
+    const packageVersion = '0.1.0'
     assert.equal(Ss2Json.version(), packageVersion)
   })
 
