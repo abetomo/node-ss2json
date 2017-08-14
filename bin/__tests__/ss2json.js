13fab7e8f5f988ec9f4eec8a6bcf37f960defd7b
abetomo <abe@enzou.tokyo>
2017-08-14 10:38:12 +0900

    Add version to command line option

--- bin/__tests__/ss2json.js
+++ bin/__tests__/ss2json.js
@@ -1,6 +1,16 @@
 'use strict'
 
-/* global describe, test */
+const path = require('path')
+const execSync = require('child_process').execSync
+const ss2json = path.join(__dirname, '..', 'ss2json')
+
+/* global describe, test, expect */
 describe('bin/ss2json', () => {
-  test('Add tests')
+  test('The current version is displayed', () => {
+    const packageJson = require(path.join(__dirname, '../..', 'package.json'))
+    const ret = execSync(`node ${ss2json} --version`)
+    expect(ret.toString().trim()).toBe(packageJson.version)
+  })
+
+  test('More tests')
 })
