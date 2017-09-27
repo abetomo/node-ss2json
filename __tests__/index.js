79ae0a25a3b4f92fd7f8b4600a55e825e168e95f
abetomo <abe@enzou.tokyo>
2017-09-27 17:37:13 +0900

    Add test using mock

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,12 +1,48 @@
 'use strict'
 
-const ss2json = require('..')
+const Ss2Json = require('..')
+const ss2json = new Ss2Json()
+
+const path = require('path')
+const jsonPath = path.join('lib/google/__tests__/fixtures/auth.json')
+const testParams = {
+  authJsonPath: jsonPath,
+  spreadsheetId: 'spreadsheetId',
+  sheetName: 'sheetName',
+  values: ['dummy']
+}
 
 /* global describe, test expect */
 describe('Ss2Json', () => {
   test('ss2json is instanceOf Ss2Json', () => {
-    expect(ss2json).toBeInstanceOf(Function)
+    expect(ss2json).toBeInstanceOf(Ss2Json)
   })
 
-  test('More tests')
+  test('convert', () => {
+    return ss2json.convert(testParams).then(data => {
+      expect(data).toEqual([
+        {col1: 'a', col2: 'b', col3: 'c'},
+        {col1: 'e', col2: 'f', col3: 'g'}
+      ])
+    })
+  })
+
+  test('update', () => {
+    return ss2json.update(testParams).then(data => {
+      expect(data).toEqual({
+        spreadsheetId: 'spreadsheetId',
+        totalUpdatedRows: 1,
+        totalUpdatedColumns: 2,
+        totalUpdatedCells: 3,
+        totalUpdatedSheets: 4,
+        responses: [{
+          spreadsheetId: 'spreadsheetId',
+          updatedRange: 'name!A1:Z9',
+          updatedRows: 5,
+          updatedColumns: 5,
+          updatedCells: 7
+        }]
+      })
+    })
+  })
 })
