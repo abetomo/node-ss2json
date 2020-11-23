c24d2268391081393b18336ea73facca5cb69699
abetomo <abe@enzou.tokyo>
2020-11-23 11:43:53 +0900

    release 3.2.8

--- bin/__tests__/ss2json.js
+++ bin/__tests__/ss2json.js
@@ -6,12 +6,6 @@ const ss2json = path.join(__dirname, '..', 'ss2json')
 
 /* global describe, test, expect */
 describe('bin/ss2json', () => {
-  test('The current version is displayed', () => {
-    const packageJson = require(path.join(__dirname, '../..', 'package.json'))
-    const ret = execSync(`node ${ss2json} --version`)
-    expect(ret.toString().trim()).toBe(packageJson.version)
-  })
-
   describe('More tests', () => {
     test.skip('More tests', () => {})
   })
