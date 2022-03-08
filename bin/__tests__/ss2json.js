108cfe7a8376961cdb5c3bd4320de420d9f76aa2
abetomo <abe@enzou.tokyo>
2022-03-08 09:55:28 +0900

    fix: code scanning alerts
    ```
    This shell command depends on an uncontrolled absolute path.
    ```

--- bin/__tests__/ss2json.js
+++ bin/__tests__/ss2json.js
@@ -1,14 +1,16 @@
 'use strict'
 
 const path = require('path')
-const execSync = require('child_process').execSync
-const ss2json = path.join(__dirname, '..', 'ss2json')
+const { execFileSync } = require('child_process')
+const ss2jsonBin = path.join(__dirname, '..', 'ss2json')
 
 /* global describe, test, expect */
 describe('bin/ss2json', () => {
   test('The current version is displayed', () => {
     const packageJson = require(path.join(__dirname, '../..', 'package.json'))
-    const ret = execSync(`node ${ss2json} --version`)
+    const cmd = 'node'
+    const args = [ss2jsonBin, '--version']
+    const ret = execFileSync(cmd, args)
     expect(ret.toString().trim()).toBe(packageJson.version)
   })
 
